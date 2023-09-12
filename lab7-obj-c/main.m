f1bf33949e9fb77fea4503dfb6b46a1c05fff01f
Ali <alirezamonfared813@gmail.com>
2023-09-12 09:04:14 -0700

    s

--- lab7-obj-c/main.m
+++ lab7-obj-c/main.m
@@ -7,10 +7,50 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
-  }
-  return 0;
+@interface NSString (PigLatin)
+- (NSString *)stringByPigLatinization;
+@end
+
+@implementation NSString (PigLatin)
+
+- (NSString *)stringByPigLatinization {
+    // Create a character set for vowels to identify vowels
+    NSCharacterSet *vowelsCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@"AEIOUaeiou"];
+    
+    // Split the input string into words using whitespace as the delimiter
+    NSArray *words = [self componentsSeparatedByString:@" "];
+    NSMutableArray *pigLatinWords = [NSMutableArray arrayWithCapacity:[words count]];
+    
+    for (NSString *word in words) {
+        NSRange rangeOfConsonants = [word rangeOfCharacterFromSet:vowelsCharacterSet options:NSCaseInsensitiveSearch];
+        
+        if (rangeOfConsonants.location != NSNotFound) {
+            // Move the consonants (if any) to the end of the word
+            NSString *prefix = [word substringToIndex:rangeOfConsonants.location];
+            NSString *suffix = [word substringFromIndex:rangeOfConsonants.location];
+            
+            NSString *pigLatinWord = [NSString stringWithFormat:@"%@%@ay", suffix, prefix];
+            [pigLatinWords addObject:pigLatinWord];
+        } else {
+            // If the word contains no vowels, just append "ay"
+            [pigLatinWords addObject:[word stringByAppendingString:@"ay"]];
+        }
+    }
+    
+    // Join the Pig Latin words with spaces
+    NSString *pigLatinString = [pigLatinWords componentsJoinedByString:@" "];
+    
+    return pigLatinString;
 }
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSString *inputString = @"Kale Chips";
+        NSString *pigLatinString = [inputString stringByPigLatinization];
+        NSLog(@"%@", pigLatinString); // Output: Alekay Ipschay
+    }
+    return 0;
+}
+
