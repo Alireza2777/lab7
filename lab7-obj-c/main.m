1dfef6610f9e6c3b47ade4fb857003fbb5ebecab
Ali <alirezamonfared813@gmail.com>
2023-09-12 08:56:17 -0700

    Initial Commit

---
+++ lab7-obj-c/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lab7-obj-c
+//
+//  Created by Alireza Karimi on 2023-09-12.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
